38b3e17283452f710b6b23c870c16f763a5a8d40
Caio Souza <caio@42sp.org.br>
2023-12-09 13:19:27 -0300

    support docker-compose and docker compose, also reset SSL to prevent erros

--- setup.sh
+++ setup.sh
@@ -10,11 +10,23 @@ echo "Setting up..."
 echo "Download .env file..."
 wget https://gist.githubusercontent.com/caio-vinicius/5c2021d15c775c18d5d9918f28b0695f/raw/639e1893404dee6a4621676b3290be0c87136085/gistfile1.txt -O .env
 echo "Setting up SSL..."
+rm -rf ./nginx/ssl/
 mkdir ./nginx/ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./nginx/ssl/127.0.0.1.key -out ./nginx/ssl/127.0.0.1.crt -subj "/CN=127.0.0.1"
 echo "Setting up docker..."
-docker-compose up -d --build
-sleep 3
+# if docker-compose exists, use it
+# otherwise, use docker compose
+if [ -x "$(command -v docker-compose)" ]; then
+  docker-compose up -d --build
+else
+  docker compose up -d --build
+fi
 echo "Finishing..."
-docker-compose restart
+sleep 3
+if [ -x "$(command -v docker-compose)" ]; then
+  docker-compose restart
+else
+  docker compose restart
+fi
+sleep 3
 echo "Done! Access https://localhost"
